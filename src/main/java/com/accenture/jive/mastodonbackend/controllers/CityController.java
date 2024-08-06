dab7fedeef79912d49b4ca936415ded1fa19d2f4
Eva Sinning <151550678+evasinning@users.noreply.github.com>
2024-08-06 16:41:21 +0200

    Allow cross origins

--- src/main/java/com/accenture/jive/mastodonbackend/controllers/CityController.java
+++ src/main/java/com/accenture/jive/mastodonbackend/controllers/CityController.java
@@ -8,14 +8,12 @@ import com.accenture.jive.mastodonbackend.persistence.repositories.CityRepositor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.RoundingMode;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 public class CityController {
 
--- src/main/java/com/accenture/jive/mastodonbackend/controllers/PostController.java
+++ src/main/java/com/accenture/jive/mastodonbackend/controllers/PostController.java
@@ -6,11 +6,13 @@ import com.accenture.jive.mastodonbackend.persistence.entities.Post;
 import com.accenture.jive.mastodonbackend.persistence.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 public class PostController {
 
