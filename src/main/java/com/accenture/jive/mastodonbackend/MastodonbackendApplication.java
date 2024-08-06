3d3a43770771de543d24cb125a001fedfb037ca2
Eva Sinning <151550678+evasinning@users.noreply.github.com>
2024-08-06 11:23:46 +0200

    Initial commit

---
+++ src/main/java/com/accenture/jive/mastodonbackend/MastodonbackendApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.jive.mastodonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastodonbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastodonbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/jive/mastodonbackend/MastodonbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.jive.mastodonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastodonbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
